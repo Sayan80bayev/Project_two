f6b52c315bdc1242ce3999093a392fa95a7c3158
Sayan80bayev <sayan123serv@gamil.com>
2023-05-06 04:31:39 +0600

    Nothing special

--- fullGame.php
+++ fullGame.php
@@ -147,31 +147,31 @@
 
       <script>
       //button-back
-      function goBack() {
-        window.history.back();
-      }
+        function goBack() {
+          window.history.back();
+        }
 
-      //carousel-moving
-        let offset = 0;
-        const sliderLine = document.querySelector('.carousel-line');
-        const width = 1003;
-        console.log(width);
+        //carousel-moving
+          let offset = 0;
+          const sliderLine = document.querySelector('.carousel-line');
+          const width = 1003;
+          console.log(width);
 
-        document.querySelector('.slider-next').addEventListener('click', function(){
-            offset = offset + width;
-            if (offset > width*2) {
-                offset = 0;
-            }
-            sliderLine.style.left = -offset + 'px';
-        });
+          document.querySelector('.slider-next').addEventListener('click', function(){
+              offset = offset + width;
+              if (offset > width*2) {
+                  offset = 0;
+              }
+              sliderLine.style.left = -offset + 'px';
+          });
 
-        document.querySelector('.slider-prev').addEventListener('click', function () {
-            offset = offset - width;
-            if (offset < 0) {
-                offset = width*2;
-            }
-            sliderLine.style.left = -offset + 'px';
-        });
+          document.querySelector('.slider-prev').addEventListener('click', function () {
+              offset = offset - width;
+              if (offset < 0) {
+                  offset = width*2;
+              }
+              sliderLine.style.left = -offset + 'px';
+          });
       </script>
 </body>
 </html>
