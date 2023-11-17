4340ec6ee1047574b69f5cb32517f254a9ea8530
Sayan80bayev <sayan123serv@gmail.com>
2023-11-17 16:58:37 +0600

    poh

--- Category.php
+++ Category.php
@@ -66,7 +66,7 @@
                         </div>
                     <?php
                     }
-                } else {
+                }else {
                     ?>
                     <div class="game-card">
                         <img src="<?= $games[$i]['photo'] ?>" alt="">
