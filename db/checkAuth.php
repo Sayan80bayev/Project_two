29e342113a17da66d67fd4a089a4e1751586b152
Sayan80bayev <sayan123serv@gmail.com>
2023-11-24 11:03:13 +0600

    oKAY

--- db/checkAuth.php
+++ db/checkAuth.php
@@ -1,6 +1,6 @@
 <?php
     //A php.code that checks for status
-    if(isset($_SESSION['user_name']) && !preg_match("/^[a-zA-Z0-9]+$/", $_SESSION['user_name']))
+    if(isset($_SESSION['user_name']) && preg_match("/^[a-zA-Z0-9]+$/", $_SESSION['user_name']))
         $name = $_SESSION['user_name'];
     elseif(isset($_SERVER['user_name']) && !preg_match("/^[a-zA-Z0-9]+$/", $_SESSION['user_name']) ){
         $_SESSION['message'] = 'Warning: Incorrect user name';
