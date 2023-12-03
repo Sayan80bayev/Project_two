77ebf31966848b5d10784ae6fc7184ec726bd4dc
Sayan80bayev <sayan123serv@gmail.com>
2023-12-03 23:58:01 +0600

    fixes

--- profile/profile.php
+++ profile/profile.php
@@ -31,7 +31,7 @@
                 <ul>
                     <li class="btn"><a href="accounteditform.php">Edit profile</a></li>
                     <li class="btn"><a href="">Screenshots</a></li>
-                    <li class="btn"><a href="">Reviews</a></li>
+                    <li class="btn"><a href="Reviews.php">Reviews</a></li>
                     <li class="btn"><a href="">Illustrations</a></li>
                     <li class="btn"><a href="changepasswordform.php">Change Password</a></li>
                     <li class="btn"><a href="../logout.php">Logout</a></li>
